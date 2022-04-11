d150a69d6c941bb789192b83114edbcaf213e634
Streampunk <glad.car1474@fastmail.com>
2022-04-11 21:45:51 +0200

    basic structure

---
+++ mspace.py
@@ -0,0 +1,12 @@
+from quart import Quart, render_template, websocket
+
+app = Quart(__name__)
+
+
+@app.route("/")
+async def hello():
+    return await render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
