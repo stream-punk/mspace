318f3f92226dfca401b7dc549ae361b6b05d6055
Streampunk <glad.car1474@fastmail.com>
2022-04-11 22:30:33 +0200

    counter code

--- mspace.py
+++ mspace.py
@@ -1,11 +1,20 @@
+import aiofiles
+import toml
 from quart import Quart, render_template, websocket
 
 app = Quart(__name__)
 
 
 @app.route("/")
-async def hello():
-    return await render_template("index.html")
+async def index():
+    async with aiofiles.open("database/file.toml", mode="r", encoding="UTF-8") as f:
+        data = await f.read()
+    entry = toml.loads(data)
+    entry["counts"]["view"] += 1
+    data = toml.dumps(entry)
+    async with aiofiles.open("database/file.toml", mode="w", encoding="UTF-8") as f:
+        await f.write(data)
+    return await render_template("index.html", **entry)
 
 
 if __name__ == "__main__":
