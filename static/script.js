72b0d6e349daebe05f69275f35c9e1db78a63b98
Streampunk <glad.car1474@fastmail.com>
2022-04-14 15:24:48 +0200

    detect touch and ios

--- static/script.js
+++ static/script.js
@@ -1,6 +1,6 @@
 report_play_done = false;
 function report_play(url) {
-    if(report_play_done) return;
+    if (report_play_done) return;
     var oReq = new XMLHttpRequest();
     oReq.open("GET", url);
     oReq.send();
@@ -19,5 +19,34 @@ function play() {
         stream.play();
     } else {
         stream.pause();
+    }
+}
+
+function is_ios() {
+    return [
+        'iPad Simulator',
+        'iPhone Simulator',
+        'iPod Simulator',
+        'iPad',
+        'iPhone',
+        'iPod'
+    ].includes(navigator.platform)
+    || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+}
+
+function is_touch_enabled() {
+    return ( 'ontouchstart' in window ) ||
+           ( navigator.maxTouchPoints > 0 ) ||
+           ( navigator.msMaxTouchPoints > 0 );
+}
+
+function page_load() {
+    var stream = document.getElementById("stream");
+    if (is_ios()) {
+        stream.style.width = "30vmin";
+    } else if (is_touch_enabled()) {
+        stream.style.width = "60vmin";
+    } else {
+        stream.style.width = "45vmin";
     }
 }
